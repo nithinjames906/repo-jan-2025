a5fedd27acbce5aa637fee505e032602812ba76c
alexobsqura <alex.obsqura@gmail.com>
2025-03-05 08:53:24 +0530

    added mongo auth

--- initDB.js
+++ initDB.js
@@ -4,8 +4,8 @@ module.exports = () => {
   mongoose
     .connect(process.env.MONGODB_URI, {
       dbName: process.env.DB_NAME,
-      // user: process.env.DB_USER,
-      // pass: process.env.DB_PASS,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
